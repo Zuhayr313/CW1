e8c567ee53cbbc393fe2b5e4ca7b26d6761749e9
Zuhayr313 <ZUMAR201@caledonian.ac.uk>
2022-11-14 20:35:30 +0000

    back to original code

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -37,8 +37,6 @@ public class CurrencyConverter {
                                 break;
                 }
                 System.out.println("Thank you for using the converter.");
-                System.out.println("Thank you for using the converter2.");
-  
       }
 
 }
