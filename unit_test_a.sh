20c5969767c898706da65988fa8e4cda2284bd48
Zuhayr313 <80329199+Zuhayr313@users.noreply.github.com>
2022-11-16 10:11:46 +0000

    Update unit_test_a.sh

--- unit_test_a.sh
+++ unit_test_a.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 echo "Unit Test A - Invaild Input Tests"
+echo "---------------------------------"
 
 echo "Testing with no input data"
+echo "--------------------------"
 
 output=$(java CurrencyConverter )
 
@@ -10,14 +12,17 @@ if ["$output" = "No Input Data Provided"];
 
 then
 echo "Passed, Program exits without error"
+echo "-----------------------------------"
 
 else
 echo "Failed, Program exits with exception thrown as no input data was provided"
+echo "-------------------------------------------------------------------------"
 exit 1
 
 fi
 
 echo "Testing with wrong format of input data"
+echo "---------------------------------------"
 
 output=$(java CurrencyConverter dollars 10)
 
@@ -25,11 +30,14 @@ if ["$output" = "Invaild input data provided. Please run again with input data i
 
 then
 echo "Passed, Program exits without error"
+echo "-----------------------------------"
 
 else
 echo "Failed, Program exits with exception thrown as intput data was entered in the wrong format/order"
+echo "------------------------------------------------------------------------------------------------"
 exit 1
 
 fi
 
 echo "Invaild Input Test Completed Sucessfully"
+echo "----------------------------------------"
