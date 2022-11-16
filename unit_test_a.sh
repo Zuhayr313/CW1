9a9ec78f607dfeca1f8e101d60459ec0e38009fe
Zuhayr313 <80329199+Zuhayr313@users.noreply.github.com>
2022-11-16 20:45:21 +0000

    Working unit_test_a.sh

--- unit_test_a.sh
+++ unit_test_a.sh
@@ -54,35 +54,5 @@ echo "--------------------------------------------------------------------------
 
 fi
 
-
-echo "Testing with Correct input data"
-echo "--------------------------"
-
-echo "Expected output from program :10.0 Dollars = 7.4 Pounds
-10.0 Dollars = 8.8 Euros
-Thank you for using the converter."
-echo "-------------------------------------------------------"
-
-#Stores output of program with correct input data
-output=$(java CurrencyConverter 10 dollars)
-substr1="10.0 Dollars = 7.4 Pounds"
-substr2="10.0 Dollars = 8.8 Euros"
-
-#If statement checking to see if program's actul output matches the expected output 
-if [["$output" == *"$substr1"* ]];
-
-#Sends message if program's actul output equals expected output
-then
-echo "Passed, Program outputs correct currency converter values and exits without error"
-echo "---------------------------------------------------------------------------------"
-
-#Sends message if program's actul output does not equal expected output and fails the Jenkins build
-else
-echo "Failed, Program exits with exception thrown as program outputs correct currency converter values"
-echo "------------------------------------------------------------------------------------------------"
-#exit 1
-
-fi
-
 echo "Invaild Input Test Completed Sucessfully"
 echo "----------------------------------------"
