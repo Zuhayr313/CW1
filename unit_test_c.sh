0bf893fd33033f0f1e6f0f8345dcb4fb4b6a56cc
Zuhayr313 <80329199+Zuhayr313@users.noreply.github.com>
2022-11-17 09:40:58 +0000

    Update unit_test_c.sh

--- unit_test_c.sh
+++ unit_test_c.sh
@@ -6,13 +6,13 @@ echo "--------------------------------------"
 echo "Testing Dollar Conversion"
 echo "-------------------------"
 
-output=$(java CurrencyConverter 1 dollars)
-substr1="0.74 Pounds"
-substr2="0.88 Euros"
+#Getting varaibles for dollar conversion
+dOutput=$(java CurrencyConverter 1 dollars)
+dSubStr1="0.74 Pounds"
+dSubStr2="0.88 Euros"
 
-echo $output
-
-if [[ $output == *"$substr1"* ]];
+#If statement checking if dollar converts correctly into pounds and euros
+if [[ $dOutput == *"$dSubStr1"* ]] && [[ $dOutput == *"$dSubStr2"* ]];
 then
     echo "Passed, Dollar converts correctly"
     echo "---------------------------------"
@@ -22,14 +22,44 @@ else
     #exit 1
 fi
 
-str="hello world"
-substr="world"
-if [[ $str == *"$substr"* ]];
+echo "Testing Pound Conversion"
+echo "------------------------"
+
+#Getting varaibles for pound conversion
+pOutput=$(java CurrencyConverter 1 pounds)
+pSubStr1="1.36 Dollars"
+pSubStr2="1.19 Euros"
+
+#If statement checking if pound converts correctly into dollars and euros
+if [[ $pOutput == *"$pSubStr1"* ]] && [[ $pOutput == *"$pSubStr2"* ]];
 then
-    echo "String contains substring."
+    echo "Passed, Pound converts correctly"
+    echo "--------------------------------"
 else
-    echo "String does not contain substring."
+    echo "Failed, Pound does not correctly convert"
+    echo "----------------------------------------"
+    #exit 1
 fi
+
+echo "Testing Euro Conversion"
+echo "-----------------------"
+
+#Getting varaibles for euro conversion
+eOutput=$(java CurrencyConverter 1 euros)
+eSubStr1="1.13 Dollars"
+eSubStr2="0.84 Pounds"
+
+#If statement checking if euro converts correctly into dollars and pounds
+if [[ $pOutput == *"$pSubStr1"* ]] && [[ $pOutput == *"$pSubStr2"* ]];
+then
+    echo "Passed, Euro converts correctly"
+    echo "---------------------------------"
+else
+    echo "Failed, Euro does not correctly convert"
+    echo "-----------------------------------------"
+    #exit 1
+fi
+
 
 echo "Conversion Rates Testing Completed Sucessfully"
 echo "----------------------------------------------"
