fc2145227cf578d18fd5b94632a23c5802ea5ecd
Zuhayr313 <80329199+Zuhayr313@users.noreply.github.com>
2022-11-17 21:47:41 +0000

    Update CurrencyConverter.java

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -5,15 +5,16 @@ public class CurrencyConverter {
 
         public static void main(String[] args) {
                      
+                double amount, dollar, pound, code, euro;
+                DecimalFormat f = new DecimalFormat("##.##");
+                               //amount = Integer.parseInt(args[0]);
+                String currency = args[1].toLowerCase();
+                
                 if (args.length == 0){
                          System.out.println("No input data provided. Please run again with input data in correct format(Amount Currency)");
                         
                         } else { 
-                               double amount, dollar, pound, code, euro;
-                               DecimalFormat f = new DecimalFormat("##.##");
-                               //amount = Integer.parseInt(args[0]);
-                               String currency = args[1].toLowerCase();     
-                                             
+                                           
                         if (currency == "dollars" || currency == "pounds" || currency == "euros"){
                         
                                 amount = Integer.parseInt(args[0]);
