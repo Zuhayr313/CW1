95d161e2a356effede889c17bcd2003e281ca2c8
Zuhayr313 <80329199+Zuhayr313@users.noreply.github.com>
2022-11-17 09:24:27 +0000

    Update unit_test_c.sh

--- unit_test_c.sh
+++ unit_test_c.sh
@@ -10,15 +10,25 @@ output=$(java CurrencyConverter 1 dollar)
 substr1="0.74 Pounds"
 substr2="0.88 Euros"
 
+echo $output
 
-if [[ "$output" == "$substr1" ]];
+if [[ $output == *"$substr1"* ]];
 then
     echo "Passed, Dollar converts correctly"
     echo "---------------------------------"
 else
     echo "Failed, Dollar does not correctly convert"
     echo "-----------------------------------------"
-    exit 1
+    #exit 1
+fi
+
+str="hello world"
+substr="world"
+if [[ $str == *"$substr"* ]];
+then
+    echo "String contains substring."
+else
+    echo "String does not contain substring."
 fi
 
 echo "Conversion Rates Testing Completed Sucessfully"
