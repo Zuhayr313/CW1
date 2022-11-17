35065a258afea0cae9b6e4ca297dc15b13e6f13c
Zuhayr313 <80329199+Zuhayr313@users.noreply.github.com>
2022-11-17 11:26:19 +0000

    Update FixedCurrencyConverter.java

--- FixedCurrencyConverter.java
+++ FixedCurrencyConverter.java
@@ -10,8 +10,12 @@ public class CurrencyConverter {
                 DecimalFormat f = new DecimalFormat("##.##");
 
                 amount = Integer.parseInt(args[0]);
-                String currency = args[1];
-
+                String currency = args[1].toLowerCase();
+                              
+                if (args.length == 0){
+                  System.out.println("No input data provided. Please run again with input data in correct format(Amount Currency)");
+                        
+                } else if (args[1].includes("dollars") || args[1].includes("pounds") || args[1].includes("euros")){
                 // For amounts Conversion
                 switch (currency){
                         case "dollars" :
@@ -23,18 +27,21 @@ public class CurrencyConverter {
                                 break;
                         case "pounds":
                                 // For Pound Conversion
-                                dollar = amount + 1.36;
+                                dollar = amount * 1.36;
                                 System.out.println(amount + " Pounds = " + f.format(dollar) + " Dollars");
                                 euro = amount * 1.19;
                                 System.out.println(amount + " Pound = " + f.format(euro) + " Euros");
                                 break;
                         case "euros":
                                 // For Euro Conversion
-                                dollar = amount * 1.31;
+                                dollar = amount * 1.13;
                                 System.out.println(amount + " Euros = " + f.format(dollar) + " Dollars");
                                 pound = amount * 0.84;
                                 System.out.println(amount + " Euros = " + f.format(pound) + " Pounds");
                                 break;
+               
+                } else {
+                  System.out.println("Invaild input data provided. Please run again with input data in correct format(Amount Currency)");
                 }
                 System.out.println("Thank you for using the converter.");
         }
